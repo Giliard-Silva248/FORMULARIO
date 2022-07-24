1b72b29a1c75a226be78627de79b99329079250e
Giliard H. da Silva <ghssilva248@gmail.com>
2022-07-23 21:35:36 -0300

    validação-pt3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
+/* ADD MSG DE ERRO APOS OS INPUTS*/
+
+
 let res = document.querySelector('.res')
 let cartao = document.querySelector('.cartao') 
+let input = document.querySelectorAll('input')
+console.log(input);
 
 function enviar(){
+
+    let user = document.querySelector('#user').value
     let nome = document.querySelector('#nome').value
     let sobrenome = document.querySelector('#sobrenome').value
     let idade = document.querySelector('#idade').value
     let tel = document.querySelector('#tel').value
     let email = document.querySelector('#email').value
-    let user = document.querySelector('#user').value
     let bairro = document.querySelector('#bairro').value
     let cidade = document.querySelector('#cidade').value
     let rua = document.querySelector('#rua').value
     let ncasa = document.querySelector('#ncasa').value
-
-
-    
-    if(user.length < 3 || user.length > 10){
-        res.innerHTML = 'O idUser precisa conter entre 3 a 10 caractere'
-    }else{
-        res.innerHTML = 'idUser válido'
-    }
-
-
-
-
-
 
     if(nome || sobrenome || idade || tel || email || user || cidade || bairro || rua || ncasa){
 
@@ -32,23 +25,39 @@ function enviar(){
         cartao.innerHTML += `IDADE: ${idade}<br>`
         cartao.innerHTML += `Nº DE TELEFONE: ${tel}<br>`
         cartao.innerHTML += `E-MAIL: ${email} <br>`
-        cartao.innerHTML += `idUser: ${user}<br>`
         cartao.innerHTML += `ENDEREÇO: ${cidade}, ${bairro} - ${rua} ${ncasa}<br>`
+        msgUser()
+
     } else {
-        return msgErro()
+        msgErro()
+        msgUser()
     }
+}
 
-    
-    
-    
+function msgUser(){
+    let user = document.querySelector('#user').value
+    let mu = document.querySelector('.msg-error-us')
+    let msgUser = document.createElement('p')
+    let n1 = Math.round(Math.random() * (0 + 1000) + 1)
 
+    mu.insertAdjacentElement('beforeend', msgUser)
+
+    if(user.length < 3 || user.length > 10){
+        msgUser.innerHTML = 'O idUser precisa conter entre 3 a 10 caractere'
+    }else{
+        msgUser.innerHTML = 'idUser válido'
+        cartao.innerHTML += `idUser: ${user} (Nº: #${n1})<br>` 
+  
+    }
+    
+   
+}
+function msgErro(){   
+    let msg = document.createElement('div')
+    cartao.insertAdjacentElement('beforeend', msg)
+    msg.innerHTML = 'Nenhum campo pode estar vazio! <br>'
 }
 
 
-function msgErro(){
 
-    let merror = document.querySelectorAll('.msg-error');
-    merror.innerHTML= 'dawdaw'
-    merror.insertAdjacentElement('afterend', 'div'); 
 
-}
