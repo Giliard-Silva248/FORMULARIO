b47cdf321f96f4f8a199184f98ed4334a4da2b2b
Giliard H. da Silva <ghssilva248@gmail.com>
2022-07-23 23:31:53 -0300

    validação-pt4

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-/* ADD MSG DE ERRO APOS OS INPUTS*/
-
-
-let res = document.querySelector('.res')
 let cartao = document.querySelector('.cartao') 
 let input = document.querySelectorAll('input')
 console.log(input);
 
 function enviar(){
 
-    let user = document.querySelector('#user').value
-    let nome = document.querySelector('#nome').value
-    let sobrenome = document.querySelector('#sobrenome').value
-    let idade = document.querySelector('#idade').value
     let tel = document.querySelector('#tel').value
     let email = document.querySelector('#email').value
     let bairro = document.querySelector('#bairro').value
@@ -19,22 +11,69 @@ function enviar(){
     let rua = document.querySelector('#rua').value
     let ncasa = document.querySelector('#ncasa').value
 
-    if(nome || sobrenome || idade || tel || email || user || cidade || bairro || rua || ncasa){
+    if(nomeCompleto || msgUser || idade){
 
-        cartao.innerHTML += `NOME COMPLETO: ${nome} ${sobrenome}<br>`
-        cartao.innerHTML += `IDADE: ${idade}<br>`
-        cartao.innerHTML += `Nº DE TELEFONE: ${tel}<br>`
-        cartao.innerHTML += `E-MAIL: ${email} <br>`
-        cartao.innerHTML += `ENDEREÇO: ${cidade}, ${bairro} - ${rua} ${ncasa}<br>`
+        nomeCompleto()
+        idade()
         msgUser()
 
     } else {
-        msgErro()
+
         msgUser()
+        nomeCompleto()
+        idade()
+        
     }
 }
 
+function nomeCompleto(){
+
+    let nome = document.querySelector('#nome').value
+    let no = document.querySelector('.msg-error-no')
+    let msgNome = document.createElement('p')
+    no.insertAdjacentElement('beforeend', msgNome)
+
+    let sobrenome = document.querySelector('#sobrenome').value
+    let sobre = document.querySelector('.msg-error-sobre')
+    let msgSobreNome = document.createElement('p')
+    sobre.insertAdjacentElement('beforeend', msgSobreNome)
+   
+    if(nome.length == 0 || sobrenome == 0){
+    msgNome.innerHTML += `não pode está vazio <br>`
+    msgSobreNome.innerHTML += `não pode está vazio <br>`
+    }
+    if(!nome.match(/^[a-zA-Z]+$/g) || !sobrenome.match(/^[a-zA-Z]+$/g)) {  
+
+        msgNome.innerHTML += `não pode ser um numero`
+        msgSobreNome.innerHTML += `não pode ser um numero`
+    }else{
+        no.remove()
+        sobre.remove()
+        cartao.innerHTML += `NOME COMPLETO: ${nome} ${sobrenome}<br>`
+
+    }
+
+}
+
+function idade(){
+
+    let idade = document.querySelector('#idade').value
+    let ida = document.querySelector('.msg-error-idade')
+    let msgIdade = document.createElement('p')
+    
+    ida.insertAdjacentElement('beforeend', msgIdade)
+
+    if(idade == 0 || idade.length == 0 || idade >= 110){
+        msgIdade.innerHTML = 'idade invalida'
+    }else{
+            ida.remove()
+            cartao.innerHTML += `IDADE: ${idade}`
+    }
+
+}
+
 function msgUser(){
+
     let user = document.querySelector('#user').value
     let mu = document.querySelector('.msg-error-us')
     let msgUser = document.createElement('p')
@@ -45,19 +84,7 @@ function msgUser(){
     if(user.length < 3 || user.length > 10){
         msgUser.innerHTML = 'O idUser precisa conter entre 3 a 10 caractere'
     }else{
-        msgUser.innerHTML = 'idUser válido'
+        mu.remove()
         cartao.innerHTML += `idUser: ${user} (Nº: #${n1})<br>` 
-  
-    }
-    
-   
+    }  
 }
-function msgErro(){   
-    let msg = document.createElement('div')
-    cartao.insertAdjacentElement('beforeend', msg)
-    msg.innerHTML = 'Nenhum campo pode estar vazio! <br>'
-}
-
-
-
-
